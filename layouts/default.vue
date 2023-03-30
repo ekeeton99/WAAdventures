d36b650edcf62f7985f4b8f1f42b1fbad28235bc
Emmett Keeton <emmaleek9@yahoo.com>
2023-03-29 21:45:18 -0700

    Imported all code

---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="app">
+    <appHeader />
+    <navBar />
+    <Nuxt />
+    <appFooter />
+  </div>
+</template>
+
+<script>
+import appHeader from '../components/appHeader.vue'
+import appFooter from '../components/appFooter.vue'
+
+export default {
+    components: {
+        appHeader,
+        appFooter
+    }
+}
+</script>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1e2123;
+}
+
+header {
+  background-color: rgb(204, 234, 250);
+}
+
+h1 {
+  padding: 1rem;
+}
+
+.main-content {
+  background-color: rgb(57, 146, 99);
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+}
+
+footer {
+  background-color: rgb(104, 94, 85);
+  color: white;
+  font-weight: 550;
+}
+
+body {
+    background-color: rgb(104, 94, 85);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,45 @@
 <template>
-  <Tutorial/>
+  <div class="main-content">
+    <h1>Welcome to WA Adventures</h1>
+      <!-- set margins & padding -->
+      <div class="m-0 p-4"> 
+        <section class="p-2">
+          Washington Adventures is a place for all explorers to discover new and
+          exciting places and activities in Washington state. Find your next
+          indoor or outdoor adventure and check The Weather Channel to help plan
+          your trip. Blah blah blah content content & more content...
+        </section>
+        <section class="p-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </section>
+      </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+  export default {
+    head() {
+      return {
+        title: "Welcome to WAAdventures",
+        meta: [
+          {
+            hid: "description",
+            name: "description",
+            content: "Discover new adventures"
+          }
+        ]
+      }
+    }
+  }
 </script>
+
+<style>
+
+</style>
+
